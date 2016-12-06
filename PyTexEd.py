80684908b826a55969304e533c52d5652df239e3
3 Node Dev <sharpebr@icloud.com>
2016-12-06 13:42:09 -0800

    fleshed out view menu

--- PyTexEd.py
+++ PyTexEd.py
@@ -19,7 +19,17 @@ edit_menu = tk.Menu(menu_bar, tearoff=0)
 edit_menu.add_command(label='Undo', accelerator='(Ctrl + Z)', compound='left')
 menu_bar.add_cascade(label='Edit', menu=edit_menu)
 
+themes_menu = tk.Menu()
+themes_menu.add_radiobutton(label='Chocolate')
+themes_menu.add_radiobutton(label='Default')
+themes_menu.add_radiobutton(label='Solorized')
+themes_menu.add_radiobutton(label='Solorized-Dark')
+themes_menu.add_radiobutton(label='Sublime')
+themes_menu.add_radiobutton(label='Tango')
+
 view_menu = tk.Menu(menu_bar, tearoff=0)
+view_menu.add_checkbutton(label='Show Line Number')
+view_menu.add_cascade(label='Themes', menu=themes_menu)
 menu_bar.add_cascade(label='View', menu=view_menu)
 
 about_menu = tk.Menu(menu_bar, tearoff=0)
