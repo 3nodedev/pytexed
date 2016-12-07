e11e93cfab41bdecaa9f525030341054aeaf6a9e
3 Node Dev <sharpebr@icloud.com>
2016-12-06 16:37:59 -0800

    implemented find function

--- PyTexEd.py
+++ PyTexEd.py
@@ -13,6 +13,49 @@ def select_all(event=None):
     content_text.tag_add('sel', '1.0', 'end')
     return "break"
 
+# Find text Functionality
+def find_text(event=None):
+    search_toplevel = tk.Toplevel(root)
+    search_toplevel.title('Find Text')
+    search_toplevel.transient(root)
+    search_toplevel.resizable(False, False)
+    tk.Label(search_toplevel, text="Find All:").grid(row=0, column=0, sticky='e')
+    search_entry_widget = tk.Entry(search_toplevel, width=25)
+    search_entry_widget.grid(row=0, column=1, padx=2, pady=2, sticky='we')
+    search_entry_widget.focus_set()
+    ignore_case_value = tk.IntVar()
+    tk.Checkbutton(search_toplevel, text='Ignore Case',
+                variable=ignore_case_value).grid(row=1, column=1, sticky='e',
+                                                 padx=2, pady=2)
+    tk.Button(search_toplevel, text="Find All", underline=0,
+              command=lambda: search_output(
+                  search_entry_widget.get(), ignore_case_value.get(),
+                  content_text, search_toplevel, search_entry_widget)
+              ).grid(row=0, column=2, sticky='e' + 'w', padx=2, pady=2)
+    def close_search_window():
+        content_text.tag_remove('match', '1.0', 'end')
+        search_toplevel.destroy()
+    search_toplevel.protocol('WM_DELETE_WINDOW', close_search_window)
+    return "break"
+
+def search_output(needle, if_ignore_case, content_text, search_toplevel, search_box):
+    content_text.tag_remove('match', '1.0', 'end')
+    matches_found = 0
+    if needle:
+        start_pos = '1.0'
+        while True:
+            start_pos = content_text.search(needle, start_pos, nocase=if_ignore_case,
+                                            stopindex='end')
+            if not start_pos:
+                break
+            end_pos = '{}+{}c'.format(start_pos, len(needle))
+            content_text.tag_add('match', start_pos, end_pos)
+            matches_found += 1
+            start_pos = end_pos
+        content_text.tag_config('match', foreground='red', background='yellow')
+    search_box.focus_set()
+    search_toplevel.title('{} matches found'.format(matches_found))
+
 # Text Widget Functionality
 def cut():
     content_text.event_generate("<<Cut>>")
@@ -77,7 +120,7 @@ edit_menu.add_command(label='Paste', accelerator='Ctrl+V', compound='left',
                       image=paste_icon, command=paste)
 edit_menu.add_separator()
 edit_menu.add_command(label='Find', accelerator='Ctrl+F', compound='left',
-                      image=search_icon)
+                      image=search_icon, command=find_text)
 edit_menu.add_separator()
 edit_menu.add_command(label='Select All', accelerator='Ctrl+A', underline=7,
                       command=select_all)
@@ -127,7 +170,7 @@ line_number_bar = tk.Text(root, width=4, padx=3, takefocus=0, border=0,
                           background='blue', state='disabled', wrap='none')
 line_number_bar.pack(side='left', fill='y')
 
-# Main Text adn Scrollbar widgets
+# Main Text and Scrollbar widgets
 content_text = tk.Text(root, wrap='word', undo=1)
 content_text.pack(expand='yes', fill='both')
 scroll_bar = tk.Scrollbar(content_text)
@@ -138,8 +181,10 @@ scroll_bar.pack(side='right', fill='y')
 
 # Redo binding issue
 content_text.bind('<Control-y>', redo)
-content_text.bind('<Control-y>', redo)
+content_text.bind('<Control-Y>', redo)
 content_text.bind('<Control-A>', select_all)
 content_text.bind('<Control-a>', select_all)
+content_text.bind('<Control-f>', find_text)
+content_text.bind('<Control-F>', find_text)
 
 root.mainloop()
